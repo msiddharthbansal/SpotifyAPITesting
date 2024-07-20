2ad2b3479f1efdb66e06c9394e6b62643583a60c
msiddharthbansal <162351707+msiddharthbansal@users.noreply.github.com>
2024-07-20 18:15:25 +0530

    Add files via upload

---
+++ build/CMakeFiles/runTests.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/sid/6D285D002D0F0514/SpotifyAPITesting/src/main.cpp" "CMakeFiles/runTests.dir/src/main.cpp.o" "gcc" "CMakeFiles/runTests.dir/src/main.cpp.o.d"
+  "/media/sid/6D285D002D0F0514/SpotifyAPITesting/src/spotify_api.cpp" "CMakeFiles/runTests.dir/src/spotify_api.cpp.o" "gcc" "CMakeFiles/runTests.dir/src/spotify_api.cpp.o.d"
+  "/media/sid/6D285D002D0F0514/SpotifyAPITesting/tests/test_spotify_api.cpp" "CMakeFiles/runTests.dir/tests/test_spotify_api.cpp.o" "gcc" "CMakeFiles/runTests.dir/tests/test_spotify_api.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/src/main.cpp.o"
+  "CMakeFiles/runTests.dir/src/main.cpp.o.d"
+  "CMakeFiles/runTests.dir/src/spotify_api.cpp.o"
+  "CMakeFiles/runTests.dir/src/spotify_api.cpp.o.d"
+  "CMakeFiles/runTests.dir/tests/test_spotify_api.cpp.o"
+  "CMakeFiles/runTests.dir/tests/test_spotify_api.cpp.o.d"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
